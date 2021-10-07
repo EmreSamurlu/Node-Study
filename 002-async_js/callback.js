9d7e6197835fbe8330827c5a5c0de81975d5c40a
Emre Samurlu <emrsmrl@gmail.com>
2021-10-07 13:03:10 +0300

    Node.js Update v1.5

--- 002-async_js/callback.js
+++ 002-async_js/callback.js
@@ -29,7 +29,7 @@ const func3 = () => {
 }
 
 func1(); */
-
+/* 
 let x = 5 ;
 console.log("1. Gelen Veri",x);
 
@@ -40,3 +40,25 @@ setTimeout(()=>{
 
 x = x + 5;
 console.log("3. Gelen Veri", x);
+ */
+
+const books = [
+    {name: 'Kitap 1', author: 'Yazar 1'},
+    {name: 'Kitap 2', author: 'Yazar 2'},
+    {name: 'Kitap 3', author: 'Yazar 3'}
+];
+
+const listBooks = () =>{
+    books.map(book => 
+        console.log(book.name))
+}
+
+// listBooks();
+
+const addBook = (newBook, callback) => {
+    books.push(newBook);
+    callback();
+};
+
+addBook({name: 'Kitap 4', author: 'Yazar 4'}, listBooks);
+
