8b31ed4de0a669dd179354e30101cee062904e50
Emre Samurlu <emrsmrl@gmail.com>
2021-10-08 20:47:46 +0300

    Node.js Update v2.3

---
+++ 006-expressjs/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Index Page')
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page')
+});
+app.get('/contact', (req, res) => {
+    res.status(200).send('Contact Page')
+});
+app.get('*', (req, res) => {
+    res.status(404).send('404 Page Not Found')
+});
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`Server Started Succesfully on port: ${port}`);
+})
